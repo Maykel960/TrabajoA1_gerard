4d177e95f7ee77e8c0d1fb75a0f4776ce7e62502
khadija-ch3 <khache3@alu.edu.gva.es>
2025-09-18 22:46:27 +0200

    creación de menú

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Inicio");
-        int a = 0; //jghgj
-        int b = 1;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("1.Crear un archivo o directorio.\n" +
+                    "2.Leer el contenido de un archivo de texto.\n" +
+                    "3. Escribir contenido en un archivo de texto existente.\n" +
+                    "4. Eliminar un archivo o directorio.\n" +
+                    "5. Comprobar si un archivo o directorio existe en la ruta proporcionada.\n" +
+                    "6. Listar el contenido de un directorio.");
+
+            int op = scanner.nextInt();
+            switch (op) {
+                case 1:
+                    System.out.println("creación de archivo o directorio");
+                    break;
+                case 2:
+                    System.out.println("Escribir contenido en un archivo de texto");
+                    break;
+                case 3:
+                    System.out.println("Escribir contenido en un archivo de texto existente");
+                    break;
+                case 4:
+                    System.out.println(" Eliminar un archivo o directorio");
+                    break;
+                case 5:
+                    System.out.println(" Comprobar si un archivo o directorio existe");
+                    break;
+                case 6:
+                    System.out.println("Listar el contenido de un directorio");
+                    break;
+                default:
+                    System.out.println("Opción no válida, intenta de nuevo");
+                    return;
+
+            }
+        }
     }
 }
